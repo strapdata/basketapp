a5cf15e550200823744571b257a9c89f557eb5b9
vroyer <vroyer@strapdata.com>
2019-10-13 19:46:13 +0200

    Add security support

--- src/main/java/com/strapdata/basketapp/ElassandraStorage.java
+++ src/main/java/com/strapdata/basketapp/ElassandraStorage.java
@@ -8,12 +8,22 @@ import com.datastax.driver.core.Session;
 import com.datastax.driver.extras.codecs.enums.EnumNameCodec;
 import com.datastax.driver.mapping.Mapper;
 import com.datastax.driver.mapping.MappingManager;
+import com.strapdata.basketapp.config.ElasticsearchConfiguration;
+import com.strapdata.basketapp.model.Basket;
 import com.strapdata.basketapp.model.BasketStatus;
 import com.strapdata.basketapp.utils.DateTimeCodec;
+import com.strapdata.basketapp.utils.ElassandraSecurity;
+import io.micronaut.context.annotation.Bean;
 import io.micronaut.context.annotation.Context;
-import io.micronaut.context.annotation.Property;
+import io.micronaut.context.annotation.Factory;
 import io.micronaut.context.env.Environment;
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
 import org.elasticsearch.client.RestClient;
@@ -93,7 +103,7 @@ public class ElassandraStorage {
         }
     }
 
-    // clean data
+    // clean up data between tests
     public void cleanup() {
         try {
             if (getSession() != null) {
@@ -110,7 +120,6 @@ public class ElassandraStorage {
     public void close() {
         if (opened.compareAndSet(true, false)) {
             session.close();
-            mappers.clear();
             mappingManager = null;
             session = null;
             logger.info("Elassandra storage closed");
@@ -124,9 +133,7 @@ public class ElassandraStorage {
     @SuppressWarnings({ "unchecked", "rawtypes" })
     public <T> Mapper<T> getMapper(Class<T> clazz) {
         Objects.requireNonNull(mappingManager);
-        return mappers.computeIfAbsent(clazz, k -> {
-            return mappingManager.mapper(k);
-        });
+        return mappers.computeIfAbsent(clazz, k -> mappingManager.mapper(k));
     }
 
     public MappingManager getMappingManager() {
@@ -162,7 +169,30 @@ public class ElassandraStorage {
         RestClientBuilder clientBuilder = RestClient.builder(new HttpHost(esConfig.host, esConfig.port, esConfig.scheme));
 
         String index = KEYSPACE; // By default, index name is keyspace name
-        try(RestHighLevelClient client = new RestHighLevelClient(clientBuilder)) {
+
+        ElassandraSecurity elassandraSecurity = new ElassandraSecurity();
+        try(RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(new HttpHost(esConfig.host, esConfig.port, esConfig.scheme))
+                        .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                            @Override
+                            public HttpAsyncClientBuilder customizeHttpClient(
+                                    HttpAsyncClientBuilder httpClientBuilder) {
+                                if (elassandraSecurity.getUsername().isPresent() && elassandraSecurity.getPassword().isPresent()) {
+                                    final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+                                    credentialsProvider.setCredentials(AuthScope.ANY,
+                                            new UsernamePasswordCredentials(elassandraSecurity.getUsername().get(), elassandraSecurity.getPassword().get()));
+                                    httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                                }
+
+                                if (elassandraSecurity.getSSLContextOption().isPresent()) {
+                                    httpClientBuilder.setSSLContext(elassandraSecurity.getSSLContextOption().get());
+                                    // TODO: fix this workaround
+                                    httpClientBuilder.setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE);
+                                }
+                                return httpClientBuilder;
+                            }
+                        }))
+        ) {
             CreateIndexRequest request = new CreateIndexRequest(KEYSPACE);
             XContentBuilder mappingBuilder = XContentFactory.jsonBuilder();
             mappingBuilder.startObject();
@@ -180,12 +210,11 @@ public class ElassandraStorage {
                 .put("synchronous_refresh", true)   // for testing only
                 .build());
             CreateIndexResponse createIndexResponse = client.indices().create(request);
-            logger.info("Elasticsearch index {} created", KEYSPACE);
+            logger.info("Elasticsearch index {} created", index);
         }
     }
 
     public void initModel() throws IOException {
 
     }
-
 }
